e35bb2ef9c14e63e9ee6b971e5bcc30cc0ed8b5e
Bahrom Akramov <bahrom.akramov98@gmail.com>
2023-07-03 10:56:17 +0300

    35. Search Insert Position

--- Easy/35.SearchInsertPosition/Solution35.cs
+++ Easy/35.SearchInsertPosition/Solution35.cs
@@ -1,10 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Easy._35.SearchInsertPosition;
+﻿namespace Easy._35.SearchInsertPosition;
 public class Solution35
 {
+    public int SearchInsert(int[] nums, int target)
+    {
+        int left = 0, mid = 0, right = nums.Length - 1;
+        while (left <= right)
+        {
+            mid = (left + right) / 2;
+            if (nums[mid] == target)
+                return mid;
+            if (target > nums[mid])
+                left = mid + 1;
+            if (target < nums[mid])
+                right = mid - 1;
+        }
+        if (target < nums[nums.Length - 1])
+            return right + 1;
+        else
+            return nums.Length;
+    }
 }
