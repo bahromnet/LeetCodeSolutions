21bb9d072e3adc8147c3953f2d6fa4a4d70e5f91
Bahrom Akramov <bahrom.akramov98@gmail.com>
2023-07-07 11:52:09 +0300

    solved solution 912-exersise by buble sort algorithm but it is not good solve

---
+++ Medium/912.SortanArray/Solution912.cs
@@ -0,0 +1,21 @@
+﻿namespace Medium._912.SortanArray;
+public class Solution912
+{
+    public static int[] SortArray(int[] nums)
+    {
+        int temp = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = 0; j < nums.Length - 1; j++)
+            {
+                if (nums[j] > nums[j + 1])
+                {
+                    temp = nums[j + 1];
+                    nums[j + 1] = nums[j];
+                    nums[j] = temp;
+                }
+            }
+        }
+        return nums;
+    }
+}
