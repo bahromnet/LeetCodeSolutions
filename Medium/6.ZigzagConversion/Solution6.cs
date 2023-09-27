4c10524e45a2f3c5acea2d1eb0ffcd796636dc22
Bahrom Akramov <bahrom.akramov98@gmail.com>
2023-09-27 22:31:13 +0500

    solved Zigzag Conversion

---
+++ Medium/6.ZigzagConversion/Solution6.cs
@@ -0,0 +1,56 @@
+﻿namespace Medium._6.ZigzagConversion;
+public class Solution6
+{
+    public string Convert(string s, int numRows)
+    {
+        if (string.IsNullOrEmpty(s))
+            return "";
+
+        else if (s.Length == 1 || numRows == 1)
+            return s;
+
+        else
+        {
+            int range = 2 * numRows - 3;
+            int rangeDown = range;
+            int rangeUp = 1;
+            int rangeBetween = range;
+            int i = 0;
+            string resultStr = "";
+            while (s.Length > resultStr.Length)
+            {
+                i++;
+                if (i == 1 || i == numRows)
+                {
+                    for (int j = i - 1; j < s.Length; j += range + 1)
+                    {
+                        resultStr += s[j];
+                    }
+                }
+                else
+                {
+                    int k = 1;
+                    for (int j = i - 1; j < s.Length;)
+                    {
+                        resultStr += s[j];
+                        if (k % 2 == 1)
+                        {
+                            rangeBetween = rangeDown - 2;
+                        }
+
+                        else
+                        {
+                            rangeBetween = rangeUp;
+                        }
+                        k++;
+                        j += rangeBetween + 1;
+                    }
+                    rangeDown -= 2;
+                    rangeUp += 2;
+                    rangeBetween = range - rangeDown;
+                }
+            }
+            return resultStr;
+        }
+    }
+}
